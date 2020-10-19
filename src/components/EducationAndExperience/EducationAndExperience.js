d79e444444f895e6373769bd22ddb664a70c41ee
osterdalh <32514828+osterdalh@users.noreply.github.com>
2020-10-19 11:04:48 +0200

    eduaction and experience styled end positioned

--- src/components/EducationAndExperience/EducationAndExperience.js
+++ src/components/EducationAndExperience/EducationAndExperience.js
@@ -6,21 +6,37 @@ import ListItem from '../ListItem/ListItem'
 
 const EducationAndExperience = () => {
     return (
-        <div className="EducationAndExperience">
+        <div className="container EducationAndExperience">
             <div className="row">
                 <div className="col-6">
-                    <div className="content-fit">
-                        <Header className="" heading="Education" theme="dark"></Header>
-                        <h5>WHAT IVE LEARNED</h5>
-                        <ListItem
+                    <div className="colWidth">
+                        <Header className="" heading="Education" theme="dark">
+                            <h5>WHAT IVE LEARNED</h5>
+
+                        </Header>
+                        <ListItem className="ListItem"
                             years="2013-2018"
                             title="Norwegian University of Science and Technology - Trondheim"
                             subtitle="Master's Degree - Mechanical Engineering"></ListItem>
+                        <ListItem className="ListItem"
+                            years="2017"
+                            title="Instituto Superior Técnico - Lisbon"
+                            subtitle="Exchange Year"></ListItem>
                     </div>
+
 
                 </div>
                 <div className="col-6">
-                    <Header heading="Experience" theme="dark"></Header>
+                    <div className="colWidth float-right">
+                        <Header heading="Experience" theme="dark">
+                            <h5>WHAT IVE DONE</h5>
+
+                        </Header>
+                        <ListItem className="ListItem"
+                            years="2019 - Present"
+                            title="Cognizant - Oslo"
+                            subtitle="Frontend Developer & Business Analyst"></ListItem>
+                    </div>
 
                 </div>
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import './Header.scss'
 
-const Header = ({ heading, theme }) => {
-
+const Header = ({ heading, theme, children }) => {
     return (
         <div className={`Header ${theme}`}>
             <h1 className="pb-3 text-center position-relative">{heading}
             </h1>
             <div className="underline mb-3"></div>
-
+        <span className="text-center">{children}</span> 
         </div>
     )
 }
