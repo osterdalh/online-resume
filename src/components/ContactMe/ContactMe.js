7510d35ec53412874eaff3c3a057b1e899df186c
osterdalh <32514828+osterdalh@users.noreply.github.com>
2020-10-21 14:39:40 +0200

    from finished

--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './ContactMe.scss'
 import Header from '../Header/Header'
+import { init } from 'emailjs-com';
+import emailjs from 'emailjs-com';
 
 class ContactMe extends React.Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
+        init("user_QS9O3Kk31QdO9myDCfK84");
 
         this.state = {
             name: '',
@@ -16,35 +19,65 @@ class ContactMe extends React.Component {
     validateForm = () => {
         const { name, email, message } = this.state
         var valid = true
-        if (name.length < 3){
+        if (name.length < 3) {
             var nameElement = document.getElementById("name")
             nameElement.classList.add("notValid")
             valid = false
         }
 
-        if (email.length < 1 || !email.includes('@')){
-            var nameElement = document.getElementById("email")
-            nameElement.classList.add("notValid")
+        if (email.length < 1 || !email.includes('@')) {
+            var emailElement = document.getElementById("email")
+            emailElement.classList.add("notValid")
             valid = false
 
         }
 
-        if (message.length < 1){
-            var nameElement = document.getElementById("message")
-            nameElement.classList.add("notValid")
+        if (message.length < 1) {
+            var messageElement = document.getElementById("message")
+            messageElement.classList.add("notValid")
             valid = false
         }
 
-        if(valid){
-            this.sendForm()
+        if (valid) {
+            this.sendForm(name, email, message)
         }
-        
+
     }
 
+    showSucc = () => {
+        var nameElement = document.getElementById("name")
+        nameElement.classList.remove('notValid')
+        var emailElement = document.getElementById("email")
+        emailElement.classList.remove('notValid')
+        var messageElement = document.getElementById("message")
+        messageElement.classList.remove('notValid')
 
 
-    sendForm = () => {
-        const myEmail = "osterdalh@gmail.com"
+        var element = document.getElementById("succText")
+        element.style.visibility = "visable"
+        element.style.opacity = "1"
+    }
+
+    sendForm = (name, email, message) => {
+
+        var templateParams = {
+            name,
+            email,
+            message
+        };
+
+        emailjs.send('service_0dk9rvh', 'template_ao4gwzr', templateParams)
+            .then((response) => {
+                console.log('SUCCESS!', response.status, response.text);
+                this.setState({
+                    name: '',
+                    email: '',
+                    message: ''
+                })
+                this.showSucc()
+            }, function (error) {
+                console.log('FAILED...', error);
+            });
     }
 
 
@@ -64,19 +97,19 @@ class ContactMe extends React.Component {
                             </div>
                             <div className="col-6">
                                 <div className="formConteiner">
-                                    <label for="name">Name</label>
+                                    <label htmlFor="name">Name</label>
                                     <input id="name" onChange={(e) => this.setState({ name: e.target.value })} value={this.state.name} type="text"></input>
 
-                                    <label for="email">Email</label>
+                                    <label htmlFor="email">Email</label>
                                     <input id="email" onChange={(e) => this.setState({ email: e.target.value })} value={this.state.email} type="email"></input>
 
-                                    <label for="message">Message</label>
+                                    <label htmlFor="message">Message</label>
                                     <textarea id="message" onChange={(e) => this.setState({ message: e.target.value })} value={this.state.message} type="text"></textarea>
 
                                     <button onClick={this.validateForm}>Send</button>
+
+                                    <h5 id="succText" className="text-center pt-3 submitText">Thanks for submitting!</h5>
                                 </div>
-
-
                             </div>
                         </div>
                     </div>
