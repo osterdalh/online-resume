daa2c0531ee17e869ef93bedf9d2b818066e189e
osterdalh <32514828+osterdalh@users.noreply.github.com>
2020-10-21 15:51:23 +0200

    test publish

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,7 @@ import './Home.scss'
 const Home = () => {
     return (
         <div id="home" className="Home prallax">
-            <div className="row text-center text align-items-center">
+            <div className="row text-center text align-items-center w-100">
                 <div className="col-6 ">
                     <h1>Hans Bratland <br></br> Østerdal</h1>
                 </div>
