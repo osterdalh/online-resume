b9b3fbc1ff3a8bfd94ac4a3d5019b7c84a1190e8
osterdalh <32514828+osterdalh@users.noreply.github.com>
2020-10-22 12:13:48 +0200

    Updates

---
+++ precache-manifest.fb90806f58c0b59a3ad6221a9d779b2e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "538414044849df13f107dfe41c35dc7d",
+    "url": "/online-resume/index.html"
+  },
+  {
+    "revision": "9bdd12af86a002e82ed0",
+    "url": "/online-resume/static/css/main.677263ba.chunk.css"
+  },
+  {
+    "revision": "2d1d8413c0b7090f6a68",
+    "url": "/online-resume/static/js/2.a329741c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/online-resume/static/js/2.a329741c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bdd12af86a002e82ed0",
+    "url": "/online-resume/static/js/main.9ac681bc.chunk.js"
+  },
+  {
+    "revision": "9b137ea4bc4e7e63b1a4",
+    "url": "/online-resume/static/js/runtime-main.c5263ff3.js"
+  },
+  {
+    "revision": "94f16a16b168aa6ee86b98498f99017a",
+    "url": "/online-resume/static/media/bg.94f16a16.jpg"
+  },
+  {
+    "revision": "9dec5baa046c4277a899f39608f8f75b",
+    "url": "/online-resume/static/media/codeBackground.9dec5baa.jpg"
+  },
+  {
+    "revision": "8f956ac5f46030ae22c2112124fe1e04",
+    "url": "/online-resume/static/media/cognizant.8f956ac5.jpg"
+  },
+  {
+    "revision": "b76bea09156715890d0c09deccea0c9f",
+    "url": "/online-resume/static/media/hansAboutMe.b76bea09.jpg"
+  },
+  {
+    "revision": "3a6592468ca8efd474021c017e021778",
+    "url": "/online-resume/static/media/masters.3a659246.jpg"
+  }
+]);
