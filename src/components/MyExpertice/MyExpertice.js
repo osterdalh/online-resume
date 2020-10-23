5e53a6e0726f3dc36d3dca5c7367d119dbe4f1a2
osterdalh <32514828+osterdalh@users.noreply.github.com>
2020-10-23 13:52:34 +0200

    reverted over selector

--- src/components/MyExpertice/MyExpertice.js
+++ src/components/MyExpertice/MyExpertice.js
@@ -7,12 +7,12 @@ const MyExpertice = () => {
             <div className="container">
                 <div className="text-center">
                     <Header heading="My Expertice"></Header>
-                    <p>
+                    <p className="text-left">
                         I'm a curious person with a passion for frontend development and design.
 
                         I have a educational background from NTNU in Trondheim, with master's degree in mechanical engineering. I specialized in development of automated warehouse solutions, using computer vision, robotic manipulators, and deep learning. 
                     </p>
-                    <p>
+                    <p className="text-left">
                         I have been working as a frontend developer the last year, and have gained expertice through work related projects and personal projects. Through my work, I have used technologies and libraries such as ReactJS, AngularJS, Bootstrap, Redux ect. Select any of the beneath items from more information!
                     </p>
 
